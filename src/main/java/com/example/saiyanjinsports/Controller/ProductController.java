7b1f0a2c98020143f5e4c2f0922d517046dcb378
Hưng Cao <hungkewt@gmail.com>
2023-03-29 09:21:48 +0700

    up

--- src/main/java/com/example/saiyanjinsports/Controller/ProductController.java
+++ src/main/java/com/example/saiyanjinsports/Controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.saiyanjinsports.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class ProductController {
     @GetMapping("/all")
     public ResponseEntity<Response> getAll(){
         Response response = productService.getAll();
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Response> getOne(@PathVariable("id")long id){
+        Response response = productService.getOne(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
--- src/main/java/com/example/saiyanjinsports/Service/ProductService.java
+++ src/main/java/com/example/saiyanjinsports/Service/ProductService.java
@@ -3,6 +3,9 @@ package com.example.saiyanjinsports.Service;
 import com.example.saiyanjinsports.Entities.Product;
 import com.example.saiyanjinsports.Model.ProductDTO;
 import com.example.saiyanjinsports.Model.Response;
+import com.example.saiyanjinsports.Payload.request.CreateProduct;
+import com.example.saiyanjinsports.Payload.request.DeleteProduct;
+import com.example.saiyanjinsports.Payload.request.UpdateProduct;
 import com.example.saiyanjinsports.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,5 +40,108 @@ public class ProductService {
             return response;
         }
     }
+    public Response getOne(long id){
+        Response response = new Response();
+        try {
+            Product product = productRepository.findById(id).orElse(null);
+            if(product == null){
+                response.setStatus(404);
+                response.setMessage("Cannot found");
+                return response;
+            }
+            ProductDTO productDTO = new ProductDTO();
+            productDTO.setId(product.getId());
+            productDTO.setName(product.getPro_name());
+            productDTO.setPrice(product.getPrice());
+            productDTO.setDescription(product.getDescription());
+            productDTO.setInformation(product.getInformation());
+            response.setStatus(200);
+            response.setMessage("OK");
+            response.setData(productDTO);
+            return response;
+        }catch (Exception e){
+            response.setStatus(500);
+            response.setMessage("Internal Sever Errors");
+            return response;
+        }
+    }
 
+    public Response create(CreateProduct createProduct){
+        Response response = new Response();
+        try {
+            Product product = productRepository.findByName(createProduct.getName()).orElse(null);
+            if(product == null){
+                product = new Product();
+                product.setPro_name(createProduct.getName());
+                product.setPrice(createProduct.getPrice());
+                product.setDescription(createProduct.getDescription());
+                product.setInformation(createProduct.getInformation());
+                productRepository.save(product);
+                response.setStatus(200);
+                response.setMessage("Created");
+                return response;
+            } else {
+                response.setStatus(400);
+                response.setMessage("Exist");
+                return response;
+            }
+        }catch (Exception e){
+            response.setStatus(500);
+            response.setMessage("Internal Sever Errors");
+            return response;
+        }
+    }
+    public Response update(UpdateProduct dto){
+        Response response = new Response();
+        try {
+            Product product = productRepository.findById(dto.getId()).orElse(null);
+            if (product != null){
+                Product check = productRepository.findByName(dto.getName()).orElse(null);
+                if(check == null){
+                    product.setPro_name(dto.getName());
+                    product.setPrice(dto.getPrice());
+                    product.setDescription(dto.getDescription());
+                    product.setInformation(dto.getInformation());
+                    productRepository.save(product);
+                    response.setStatus(200);
+                    response.setMessage("OK");
+                    return response;
+                } else {
+                    response.setStatus(400);
+                    response.setMessage("Product Already Exist");
+                    return response;
+                }
+            } else {
+                response.setStatus(400);
+                response.setMessage("Does not exist");
+                return response;
+            }
+        } catch (Exception e){
+            response.setStatus(500);
+            response.setMessage("OMG!");
+            response.getError().add(e.getMessage());
+            return response;
+        }
+    }
+    public Response delete(DeleteProduct dto){
+        Response response = new Response();
+        try {
+            Product product = productRepository.findById(dto.getId()).orElse(null);
+            if(product != null){
+                product.setId(dto.getId());
+                productRepository.delete(product);
+                response.setStatus(200);
+                response.setMessage("Deleted");
+                return response;
+            }else {
+                response.setStatus(400);
+                response.setMessage("Does not exist");
+                return response;
+            }
+        } catch (Exception e){
+            response.setStatus(500);
+            response.setMessage("OMG!");
+            return response;
+        }
+    }
 }
